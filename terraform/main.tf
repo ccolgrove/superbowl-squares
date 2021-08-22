ea4b0617a5124456ca507a178a999af01459f942
Caitlin Colgrove <caitlin@hex.tech>
2021-08-22 16:36:24 -0700

    fix policy?

--- terraform/main.tf
+++ terraform/main.tf
@@ -99,7 +99,7 @@ resource "aws_s3_bucket_policy" "domain_bucket" {
         Principal = "*"
         Action    = ["s3:GetObject"]
         Resource = [
-          aws_s3_bucket.domain_bucket.arn,
+          "${aws_s3_bucket.domain_bucket.arn}/*",
         ]
       },
     ]
